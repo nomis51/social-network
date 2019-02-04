1f3831c03a473a0b0a342a2d02b0cbe0b8263fd0
Simon Thomas <nomis51@users.noreply.github.com>
2019-02-03 20:04:11 -0500

    Add binding to content prop in the postForm

--- client/src/components/PostForm/PostForm.js
+++ client/src/components/PostForm/PostForm.js
@@ -5,20 +5,35 @@ import PropTypes from 'prop-types';
 import { createPost } from '../../redux/actions/postActions';
 
 class PostForm extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            content: ''
+        };
+    }
+
     onSubmit = (e) => {
         e.preventDefault();
         const post = {
-            content: 'New post'
+            content: this.state.content
         };
 
         this.props.createPost(post);
+
+        this.setState({ content: '' });
+    }
+
+    onChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        });
     }
 
     render() {
         return (
             <form className="post-form" onSubmit={this.onSubmit}>
                 <div className="form-control">
-                    <textarea name="content" />
+                    <textarea name="content" value={this.state.content} onChange={this.onChange} />
                 </div>
                 <div className="post-action">
                     <button type="submit">Share</button>

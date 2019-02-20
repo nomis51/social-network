1d9e5a82999b09b62b09a42fdeb2049d3ba4e70f
Simon Thomas <nomis.guit.51@gmail.com>
2019-02-20 18:43:44 -0500

    Add friend online status, friend connection event and friend disconnection event in the friendList component

--- client/src/components/FriendList/FriendList.js
+++ client/src/components/FriendList/FriendList.js
@@ -1,25 +1,69 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import socketIOClient from 'socket.io-client';
 
 import { fetchFriends } from '../../redux/actions/userActions';
 
 import './FriendList.css';
 
 class FriendList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            socket: socketIOClient('http://localhost:8081', { query: `token=${sessionStorage.getItem('token')}` }),
+            content: '',
+            friendStatus: new Map()
+        };
+    }
+
     componentWillMount() {
         this.props.fetchFriends();
+    }
+
+    componentDidMount() {
+        this.setState({
+            friends: this.props.friends
+        });
+
+        this.state.socket.on('friendConnection', (friend) => {
+            const { friendStatus } = this.state;
+            if (!this.state.friendStatus.get(friend.friend_id)) {
+                friendStatus.set(friend.friend_id, 1);
+            }
+            this.setState({ friendStatus });
+        });
+
+        this.state.socket.on('friendDisconnection', (friend) => {
+            const { friendStatus } = this.state;
+            if (this.state.friendStatus.get(friend.friend_id)) {
+                this.state.friendStatus.delete(friend.friend_id);
+            }
+            this.setState({ friendStatus });
+        });
+
+        this.state.socket.on('onlineFriends', (friends) => {
+            const { friendStatus } = this.state;
+            friends.forEach(f => {
+                if (!this.state.friendStatus.get(f._id)) {
+                    this.state.friendStatus.set(f._id, 1);
+                }
+            });
+            this.setState({ friendStatus });
+        });
     }
 
     render() {
         const friends = this.props.friends.map((f, i) => {
             return (
                 <li className="friend" key={f._id}>
+                    {this.state.friendStatus.get(f._id) && <div className="online-dot"></div>}
                     <img src="https://www.shareicon.net/download/128x128//2015/10/14/656187_cat_512x512.png" alt={f.firstName + '\'s image'} />
                     <p>{f.firstName} {f.lastName}</p>
                 </li>
             );
         });
+
         return (
             <div className="friends">
                 <h3>Friends</h3>
--- server/routes/socket.io/socket.io.js
+++ server/routes/socket.io/socket.io.js
@@ -1,6 +1,7 @@
 const SocketIO = require('socket.io');
 const socketioAuth = require('./socket.io.auth');
 const messageService = require('../message/message.service');
+const userService = require('../user/user.service');
 
 module.exports = class SocketIORoute {
     constructor(server) {
@@ -12,9 +13,38 @@ module.exports = class SocketIORoute {
                 console.log('Client connected');
                 this.clients.set(client.user_id, client);
 
+                userService.getFriends(client.user_id)
+                    .then(friends => {
+                        friends.map((f, i) => {
+                            const friend = this.clients.get(f._id);
+                            if (friend) {
+                                friend.emit('friendConnection', { friend_id: client.user_id });
+                            }
+                        });
+
+                        client.emit('onlineFriends', friends.filter((f, i) => {
+                            return this.clients.get(f._id);
+                        })
+                            .map((f, i) => {
+                                return {
+                                    _id: f._id
+                                };
+                            }));
+                    });
+
                 client.on('disconnect', () => {
                     console.log('Client disconnected');
                     this.clients.delete(client.user_id);
+
+                    userService.getFriends(client.user_id)
+                        .then(friends => {
+                            friends.map((f, i) => {
+                                const friend = this.clients.get(f._id);
+                                if (friend) {
+                                    friend.emit('friendDisconnection', { friend_id: client.user_id });
+                                }
+                            });
+                        });
                 });
 
                 //** Message **//
@@ -33,7 +63,7 @@ module.exports = class SocketIORoute {
 
                 client.on('typing', (recipient_id) => {
                     const recipient = this.clients.get(recipient_id);
-                    
+
                     if (recipient) {
                         recipient.emit('recipientTyping');
                     }
